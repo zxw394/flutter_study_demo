c8f83fd7d7ce5628e253781e45f5274f118ea63a
zxw394 <zhangxwforload@126.com>
2019-09-17 17:29:39 +0800

    添加main.dart

---
+++ main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(wordPair.asPascalCase + "111"),
+        ),
+        body: new Center(
+          child: new RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+class RandomWords extends StatefulWidget{
+  @override
+  createState() => new RandomWordsState();
+}
+class RandomWordsState extends State<RandomWords>{
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
+  }
+}
